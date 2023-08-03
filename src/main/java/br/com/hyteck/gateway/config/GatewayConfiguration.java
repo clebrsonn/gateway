2dc662a1bd20e86d3f3ed33962a45b3b2ca1e33b
cleberson.chagas <cleberson.chagas@syngenta.com>
2023-08-03 16:01:54 -0300

    initial configuration

---
+++ src/main/java/br/com/hyteck/gateway/config/GatewayConfiguration.java
@@ -0,0 +1,20 @@
+package br.com.hyteck.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+    @Bean
+    public RouteLocator customRouterLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("cloud-config", r -> r
+                        .path("/config/**")
+                                .filters(f -> f.rewritePath("/config/(?<params>.*)","/${params}" ))
+                        .uri("lb://configServer")
+                        )
+                .build();
+    }
+}
